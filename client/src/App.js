70289be54b5b73ea3e17ebfc9d2d8b36a3898669
2Zerozero <hawgae@gmail.com>
2023-01-06 17:34:53 +0900

    ✨ feat: footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,33 @@
 import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Main from "./pages/Main/Main";
+import { createGlobalStyle } from "styled-components";
+import { BrowserRouter } from "react-router-dom";
+
+
+const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    list-style: none;
+    text-decoration: none;
+    font-family: "Noto Sans KR";
+  }
+`;
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <GlobalStyle />
+        <BrowserRouter>
+          <Header />
+          <Main />
+          <Footer />
+        </BrowserRouter>
+    </>
+  )
 }
 
 export default App;
