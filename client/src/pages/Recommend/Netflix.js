88afb53b67769a16a5e1cfd26373cb9d4c500398
kangseong-sim <stylesha34@gmail.com>
2023-01-07 18:04:18 +0900

    ♻️ refactor: Recommend page 더미데이터 추가 후 UI 수정

--- client/src/pages/Recommend/Netflix.js
+++ client/src/pages/Recommend/Netflix.js
@@ -1,12 +1,8 @@
 import React from "react";
 import styled from "styled-components";
+import dummy from "./dummydata";
 
 const ContentList = styled.div`
-  width: 100%;
-  height: 100vh;
-`;
-
-const ContentItemDiv = styled.div`
   width: 100%;
   height: auto;
   padding-top: 10px;
@@ -28,53 +24,31 @@ const ContentItem = styled.div`
     background: #58bfad;
     border-radius: 30px;
   }
+  .memberPicture {
+    border-radius: 30px;
+    width: 48px;
+  }
 `;
 
 const Netflix = () => {
   return (
     <ContentList>
-      <ContentItemDiv>
-        <ContentItem>
-          <div className="userInfo">김희진</div>
-          <div className="content">넷플릭스 “에밀리 파리에 가다" 추천해요!</div>
-        </ContentItem>
-      </ContentItemDiv>
-      <ContentItemDiv>
-        <ContentItem>
-          <div className="userInfo">강성심</div>
-          <div className="content">넷플릭스 “더 글로리" 꿀 잼</div>
-        </ContentItem>
-      </ContentItemDiv>
-      <ContentItemDiv>
-        <ContentItem>
-          <div className="userInfo">이영우</div>
-          <div className="content">넷플릭스 “외계+인 1부" 별로에요...</div>
-        </ContentItem>
-      </ContentItemDiv>
-      <ContentItemDiv>
-        <ContentItem>
-          <div className="userInfo">장한나</div>
-          <div className="content">
-            넷플릭스 “에밀리 파리에 가다" 추천해요!{" "}
-          </div>
-        </ContentItem>
-      </ContentItemDiv>
-      <ContentItemDiv>
-        <ContentItem>
-          <div className="userInfo">박금비</div>
-          <div className="content">
-            넷플릭스 “시스피라시” 문제작... 꼭 보세요{" "}
-          </div>
-        </ContentItem>
-      </ContentItemDiv>
-      <ContentItemDiv>
-        <ContentItem>
-          <div className="userInfo">강신찬</div>
-          <div className="content">
-            넷플릭스 “오징어 게임” 내년에 공개한데요 !
-          </div>
-        </ContentItem>
-      </ContentItemDiv>
+      {dummy.map((item) => {
+        return (
+          <ContentItem key={item.id}>
+            <div className="userInfo">
+              <img
+                src={item.memberPicture}
+                className="memberPicture"
+                alt="사용자 이미지"
+                style={{}}
+              ></img>
+              {item.name}
+            </div>
+            <div className="content">{item.commentBody}</div>
+          </ContentItem>
+        );
+      })}
     </ContentList>
   );
 };
