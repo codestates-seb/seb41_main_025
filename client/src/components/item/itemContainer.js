9b059771e6fbd24e33f81db5471c880506914ab9
2Zerozero <hawgae@gmail.com>
2023-01-10 14:32:18 +0900

    ♻️ refactor: 컴포넌트 분리

---
+++ client/src/components/item/itemContainer.js
@@ -0,0 +1,102 @@
+import styled, { css } from "styled-components";
+import Item from "./item";
+import { useRef, useCallback } from "react";
+import { Link } from "react-router-dom";
+
+// slide
+import Slider from "react-slick";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  margin-top: 40px;
+
+  h2.title { 
+    margin-bottom: 14px; 
+  }
+  .items {
+    display: flex;
+    overflow: hidden;
+  }
+
+  .item {
+    margin-right: 30px;
+  }
+`
+
+const defaultButtonStyle = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: calc(50% - 30px);
+  translate: (-50%, -50%);
+  padding: 0;
+  width: 40px;
+  height: 40px;
+  border: none;
+  border-radius: 100%;
+  background: #D9D9D9;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  cursor: pointer;
+`;
+
+const PrevButton = styled.button`
+  ${defaultButtonStyle}
+  left: -1.5%;
+`;
+
+const NextButton = styled.button`
+  ${defaultButtonStyle}
+  right: 0;
+`;
+
+const ItemContainer = () => {
+  //React-slick
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 1000,
+    //autoplay: true,
+    autoplayspeed: 1000,
+    arrows: false,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+  };
+
+  // prev, next
+  const slickRef = useRef(null);
+  const previous = useCallback(() => slickRef.current.slickPrev(), []);
+  const next = useCallback(() => slickRef.current.slickNext(), []);
+  
+  return (
+    <Container>
+      <h2 className="title">박스오피스 순위</h2>
+      <Slider className="items" ref={slickRef} {...settings}>
+        {/* item.map(item => ()) */}
+        <Link to="detail"><Item /></Link>
+        <Link to="detail"><Item /></Link>
+        <Link to="detail"><Item /></Link>
+        <Link to="detail"><Item /></Link>
+        <Link to="detail"><Item /></Link>
+        <Link to="detail"><Item /></Link>
+        <Link to="detail"><Item /></Link>
+        <Link to="detail"><Item /></Link>
+        <Link to="detail"><Item /></Link>
+        <Link to="detail"><Item /></Link>
+        <Link to="detail"><Item /></Link>
+      </Slider>
+      <PrevButton onClick={previous}>
+        <img src="/assets/ArrowPrev.svg" alt="이전" />
+      </PrevButton>
+      <NextButton onClick={next}>
+        <img src="/assets/ArrowNext.svg" alt="다음" />
+      </NextButton>
+    </Container>
+  )
+}
+
+export default ItemContainer;
