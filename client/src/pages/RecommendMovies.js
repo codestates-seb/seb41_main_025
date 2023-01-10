ed70efc39c082dc6b6321bf014677bf91c84c7ee
janghanna <ewal4291@gmail.com>
2023-01-10 11:27:22 +0900

    ♻️ refactor: 사용하지 않는 코드 정리

--- client/src/pages/RecommendMovies.js
+++ client/src/pages/RecommendMovies.js
@@ -2,14 +2,8 @@ import styled from "styled-components";
 import { MainWarp, MainContainer } from "./Main/Main";
 import { Items, Title } from "./FavoriteMovie";
 import SeleteItem from "../components/item/SeleteItem" 
-import { ButtonForm } from "../components/item/Button";
 import { useState } from "react";
 
-const Nev = styled.div`
-    display: flex;
-    justify-content: left;
-    margin: 40px;
-`
 
 const RecommendButton = styled.button`
     display: grid;
@@ -21,9 +15,9 @@ const RecommendButton = styled.button`
     margin-left: 60px;
     background-color: #167E6C;
     border-radius: 15px;
+    border: 0;
     color: white;
     font-size: 20px;
-    border: 0;
     :active {
         color: #167E6C;
         background-color: whitesmoke;
@@ -43,9 +37,9 @@ const RecommendMovies = () => {
             <MainContainer>
                 <Title style={{"marginBottom" : 0}}>
                     <span className="title">00님이 추천/비추천 한 작품</span>
-                    </Title>
-                    <RecommendButton onClick={ButtonHandleClick}>{state ? "내가 추천한 작품" : " 내가 비 추천한 작품" }</RecommendButton>
-                    {state ? (
+                </Title>
+                <RecommendButton onClick={ButtonHandleClick}>{state ? "내가 추천한 작품" : " 내가 비 추천한 작품" }</RecommendButton>
+                {state ? (
                     <Items>
                         {/*TODO: 추천 값이 true인 것들을 filter 해서 뿌려주기 */}
                         <SeleteItem/>
