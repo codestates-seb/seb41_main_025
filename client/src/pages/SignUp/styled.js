59860ec971f9402741c7da37075f9c906f906920
kangseong-sim <stylesha34@gmail.com>
2023-01-13 22:10:21 +0900

    🎨 design: 회원가입 페이지 UI 수정

--- client/src/pages/SignUp/styled.js
+++ client/src/pages/SignUp/styled.js
@@ -1,18 +1,27 @@
 import styled from "styled-components";
+import { Whitebutton, Enter, Window } from "../Login/styled";
+
+export const WindowDiv = styled(Window)`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  /* margin: auto; */
+`;
 
 export const Hello = styled.div`
-    display: flex;
-    text-align: left;
-    margin: auto;
-    
-`
+  display: flex;
+  text-align: left;
+  /* margin: 0auto; */
+`;
 export const HelloAnnouncement = styled.span`
-    margin-top: 60px;
-    margin-right: 90px;
-    font-size: 30px;
-    
-    @media only screen and (max-width: ${'600px'}) {
-        font-size: 20px;
-        margin-right: 50px;
-    }
-`
+  /* margin-top: 60px; */
+  /* margin-right: 90px; */
+  font-size: 30px;
+
+  @media only screen and (max-width: ${"600px"}) {
+    font-size: 20px;
+    margin-right: 50px;
+  }
+`;
+export const SignUpButton = styled(Whitebutton)``;
+
