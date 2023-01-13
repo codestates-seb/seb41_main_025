be4d2cd855b6994319ac972cb0b235859a88fdbf
keumbi <rmaqldi@gmail.com>
2023-01-13 10:11:22 +0900

    ✨ feat: oauth

--- server/src/main/java/com/mainProject/server/domain/member/service/MemberService.java
+++ server/src/main/java/com/mainProject/server/domain/member/service/MemberService.java
@@ -13,7 +13,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
