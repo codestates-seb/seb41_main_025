c2e506a4f2130b59d3f79c6ae1b547adf3927737
keumbi <rmaqldi@gmail.com>
2023-01-13 17:17:53 +0900

    🐛 fix: cros error

--- server/src/main/java/com/mainProject/server/global/auth/config/SecurityConfiguration.java
+++ server/src/main/java/com/mainProject/server/global/auth/config/SecurityConfiguration.java
@@ -21,9 +21,6 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.util.Arrays;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity(debug = true) //테스트 용으로
