b10c04e33efad618ee14fa258a764f3deccca0b2
janghanna <ewal4291@gmail.com>
2023-01-13 16:55:33 +0900

    🐛 fix: cors 에러로 인한 proxy

---
+++ client/src/setUpProxy.js
@@ -0,0 +1,10 @@
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+
+// module.exports = function (app) {
+//   app.use(
+//     createProxyMiddleware('/members', {
+//       target: "http://whatu1.kro.kr:8080/",
+//       changeOrigin: true,
+//     }),
+//   );
+// };
