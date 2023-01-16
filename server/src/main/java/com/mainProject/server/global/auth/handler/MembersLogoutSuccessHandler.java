e58c24dd96e9d570398c277913dfe745ea963f33
keumbi <rmaqldi@gmail.com>
2023-01-16 10:58:03 +0900

    ✨ feat:logout

---
+++ server/src/main/java/com/mainProject/server/global/auth/handler/MembersLogoutSuccessHandler.java
@@ -0,0 +1,18 @@
+package com.mainProject.server.global.auth.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+public class MembersLogoutSuccessHandler implements LogoutSuccessHandler {
+    @Override
+    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        log.info("# logout successfully!");
+    }
+}
