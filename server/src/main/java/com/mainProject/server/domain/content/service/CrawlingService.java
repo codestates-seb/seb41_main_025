9ff806d2690810a81ee32794cc3b3272e3111809
superscman <vdcf2000@gmail.com>
2023-01-17 14:30:46 +0900

    🐛 fix: 평점 러닝타임 순서 변경

--- server/src/main/java/com/mainProject/server/domain/content/service/CrawlingService.java
+++ server/src/main/java/com/mainProject/server/domain/content/service/CrawlingService.java
@@ -62,8 +62,8 @@ public class CrawlingService {
             String contentGenre = movieList.get(1); // 장르 1
             String contentCountry = movieList.get(2); // 국가 2
             String contentGrade = movieList.get(3); // 등급 3
-            String contentRunningTime = movieList.get(4); // 러닝타임 4
-            String contentScore = movieList.get(5); // 평점5
+            String contentScore = movieList.get(4); // 평점4
+            String contentRunningTime = movieList.get(5); // 러닝타임 5
             String contentAttendance = movieList.get(6); // 누적관객6
             String contentTitle = movieList.get(7); // 타이틀 7
             String contentBody = movieList.get(8); // desc 8
@@ -74,8 +74,8 @@ public class CrawlingService {
             content.setContentGenre(contentGenre); // 장르 1
             content.setContentCountry(contentCountry);// 국가 2
             content.setContentGrade(contentGrade); // 등급 3
-            content.setContentRunningTime(contentRunningTime); // 러닝타임 4
-            content.setContentScore(contentScore); // 평점5
+            content.setContentScore(contentScore); // 평점4
+            content.setContentRunningTime(contentRunningTime); // 러닝타임 5
             content.setContentAttendance(contentAttendance); // 누적관객6
             content.setContentTitle(contentTitle); // 타이틀 7
             content.setContentBody(contentBody); // desc8
@@ -135,8 +135,8 @@ public class CrawlingService {
         result.add(infoMap.get("장르"));
         result.add(infoMap.get("국가"));
         result.add(infoMap.get("등급"));
-        result.add(infoMap.get("러닝타임"));
         result.add(infoMap.get("평점"));
+        result.add(infoMap.get("러닝타임"));
         result.add(infoMap.get("누적관객"));
         result.add(titleList);
         result.add(descList);
