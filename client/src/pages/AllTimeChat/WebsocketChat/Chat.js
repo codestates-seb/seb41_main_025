4d6bbdd6f9c06815814166e02e1e19a4b9c6e596
janghanna <ewal4291@gmail.com>
2023-01-21 15:49:05 +0900

    🗑️ remove: 웹소켓 삭제

--- client/src/pages/AllTimeChat/WebsocketChat/Chat.js
+++ client/src/pages/AllTimeChat/WebsocketChat/Chat.js
@@ -1,16 +1,16 @@
-import SockJS from "sockjs";
+// import SockJS from "sockjs";
 
-var sock = new SockJS('https://mydomain.com/my_prefix');
-sock.onopen = function() {
-    console.log('open');
-    sock.send('test');
-};
+// var sock = new SockJS('https://mydomain.com/my_prefix');
+// sock.onopen = function() {
+//     console.log('open');
+//     sock.send('test');
+// };
 
-sock.onmessage = function(e) {
-    console.log('message', e.data);
-    sock.close();
-};
+// sock.onmessage = function(e) {
+//     console.log('message', e.data);
+//     sock.close();
+// };
 
-sock.onclose = function() {
-    console.log('close');
-};
+// sock.onclose = function() {
+//     console.log('close');
+// };
