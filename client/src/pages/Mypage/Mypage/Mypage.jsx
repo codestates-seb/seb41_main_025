e97d2103ca24a0bd0094ca1988afd7913303ba8a
janghanna <ewal4291@gmail.com>
2023-01-28 01:19:43 +0900

    ♻️ refactor: 미사용 코드 정리

--- client/src/pages/Mypage/Mypage/Mypage.jsx
+++ client/src/pages/Mypage/Mypage/Mypage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect} from "react";
 import * as S from "./styled";
 import ModalBasic from "../ModalBasic/ModalBasic";
 import axios from "axios";
