e92cca0947d0e731ce160dba6371ff6d4f308ae7
2Zerozero <hawgae@gmail.com>
2023-01-31 17:12:04 +0900

    ♻️ refactor: uesCustomQuery 사용

--- client/src/pages/Main/Main.jsx
+++ client/src/pages/Main/Main.jsx
@@ -1,13 +1,22 @@
 import * as S from "./styled";
-import useFetch from "../../components/util/useFetch";
 import ItemContainer from "../../components/item/ItemContainer/itemContainer";
+import { useCustomQuery } from "../../components/util/useCustomQuery";
+import Loading from "../../components/Loading/Loading";
+import Error from "../../components/Error/Error";
 
 const Main = () => {
-  const [movies] = useFetch('http://whatu1.kro.kr:8080/contents');
+  const { data, isLoading, error, refetch } = useCustomQuery(
+    `/contents`,
+  );
   
-  const Watcha = movies.filter( movie => movie.contentOttName === "watcha" );
-  const Tving = movies.filter( movie => movie.contentOttName === "tving" );
-  const Wavve = movies.filter( movie => movie.contentOttName === "wavve" );
+  if (error) return <Error/>;
+  if (isLoading) return <Loading />;
+  
+  const movies = [data];
+
+  const Watcha = movies[0].data.filter( movie => movie.contentOttName === "watcha" );
+  const Tving = movies[0].data.filter( movie => movie.contentOttName === "tving" );
+  const Wavve = movies[0].data.filter( movie => movie.contentOttName === "wavve" );
   
   return (
     <S.MainWarp>
