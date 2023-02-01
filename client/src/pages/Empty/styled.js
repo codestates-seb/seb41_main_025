0cbe8337b37c9e4ecefa50dd1d8aab513f6534b4
kangseong-sim <stylesha34@gmail.com>
2023-02-01 18:09:45 +0900

    💄 style: 404 화면 css 수정

--- client/src/pages/Empty/styled.js
+++ client/src/pages/Empty/styled.js
@@ -5,8 +5,6 @@ export const Main = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 60px 60px 0px;
-  height: 80vh;
   @media only screen and (max-width: ${"350px"}) {
     font-size: 15px;
     width: 150px;
