48d159f53de239c81a84d6312c0eeac94934cea4
janghanna <ewal4291@gmail.com>
2023-02-01 22:08:51 +0900

    🐛 fix: 추가되는 데이터 filtering

--- client/src/pages/Main/Main.jsx
+++ client/src/pages/Main/Main.jsx
@@ -5,7 +5,7 @@ import Loading from "../../components/Loading/Loading";
 import Error from "../../components/Error/Error";
 
 const Main = () => {
-  const { data, isLoading, error, refetch } = useCustomQuery(
+  const { data, isLoading, error } = useCustomQuery(
     `/contents`,
   );
   
@@ -13,11 +13,51 @@ const Main = () => {
   if (isLoading) return <Loading />;
   
   const movies = [data];
+  //만약에 ottrank의 같은 값이 존재한다면 modify가 최근 인걸로 출력
 
-  const Watcha = movies[0].data.filter( movie => movie.contentOttName === "watcha" );
-  const Tving = movies[0].data.filter( movie => movie.contentOttName === "tving" );
-  const Wavve = movies[0].data.filter( movie => movie.contentOttName === "wavve" );
+  const watchaMovies = movies[0].data.filter( movie => movie.contentOttName === "watcha" );
+  const tvingMovies = movies[0].data.filter( movie => movie.contentOttName === "tving" );
+  const wavveMovies = movies[0].data.filter( movie => movie.contentOttName === "wavve" );
+console.log(movies[0].data)
+  const Watcha = watchaMovies.filter((watchMovie, index) =>
+  { 
+    // console.log("index", index)
+    return ( 
+      watchaMovies.findIndex((watchMovie2,j) => 
+    { 
+    return (
+      watchMovie.contentOttRank === watchMovie2.contentOttRank )
+    }) === index
+    )
+  });
+  console.log(Watcha)
+
+  const Tving = tvingMovies.filter((tivingmovie, index) =>
+  { 
+    return ( 
+      tvingMovies.findIndex((tivingmovie2,j) => 
+      { 
+      return (
+        tivingmovie.contentOttRank === tivingmovie2.contentOttRank  )
+      }) === index
+    )
+  });
+
   
+  
+  const Wavve = wavveMovies.filter((wavveMovie, index3) =>
+  { 
+    // console.log("index3", index3)
+    return ( 
+      wavveMovies.findIndex((wavveMovie2,j) => 
+    { 
+    return (
+      wavveMovie.contentOttRank === wavveMovie2.contentOttRank )
+    }) === index3
+    )
+  });
+  console.log(Wavve)
+
   return (
     <S.MainWarp>
       <S.MainContainer>
