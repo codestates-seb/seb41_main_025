e869fc7ec5aa7ace292b452fa2f4ada762942289
janghanna <ewal4291@gmail.com>
2023-02-01 18:17:28 +0900

    ✨ feat: error 컴포넌트 수정

--- client/src/components/Error/Error.jsx
+++ client/src/components/Error/Error.jsx
@@ -9,7 +9,7 @@ return (
         <span>404 error</span>
         <p className="p-a">): Whoops </p>
         <p className="p-b">It seems like we couldn't find the page you were looking for</p>
-        {/* <a href='#' class="back">dd</a> */}
+        <a href='/' className="back">HOME</a>
         </S.ErrorText>
     </S.Error>
 
