e9cd2faad67287c3a201511abdb8125a1d4cb5ff
keumbi <rmaqldi@gmail.com>
2023-02-02 14:29:46 +0900

    ♻️ refactor: password

--- server/src/main/java/com/mainProject/server/domain/member/dto/MemberDto.java
+++ server/src/main/java/com/mainProject/server/domain/member/dto/MemberDto.java
@@ -3,9 +3,7 @@ package com.mainProject.server.domain.member.dto;
 import com.mainProject.server.global.validator.NotSpace;
 import lombok.*;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.*;
 import java.time.LocalDateTime;
 
 public class MemberDto {
